dd7559b1c46a2bee327432c721503155dfa44c2f
Neo Mauizan Ali Fitrah <jack.00pole@gmail.com>
2023-06-24 21:58:55 +0700

    ini tubes

---
+++ app/src/main/java/com/example/meshop/SignIn.kt
@@ -0,0 +1,14 @@
+package com.example.meshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        supportActionBar?.hide()
+    }
+}
